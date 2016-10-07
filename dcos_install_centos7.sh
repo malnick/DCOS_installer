4cf23423b3e0c0b71d76ccaeadb3aa07e8fa2595
Fernando Sanchez <fernando@mesosphere.io>
2016-10-07 11:57:04 -0400

    Update dcos_install_centos7.sh

--- dcos_install_centos7.sh
+++ dcos_install_centos7.sh
@@ -461,7 +461,7 @@ gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
 #install docker engine, daemon and service, along with dependencies
-sudo yum install -y docker-engine-1.11.2-1.el7.centos docker-engine-selinux-1.11.2-1.el7.centos \\
+sudo yum install -y docker-engine-1.11.2-1.el7.centos docker-engine-selinux-1.11.2-1.el7.centos \
  wget tar xz curl zip unzip ipset ntp 
 
 #configure ntp
