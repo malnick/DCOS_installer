eed8ea381ea0756b237a1e561d0c6747bfbb3b18
Fernando Sanchez <fernando@mesosphere.io>
2016-11-18 20:02:12 -0500

    Update dcos_install_rh7.sh

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -105,7 +105,6 @@ else
   MASTER_IP=`cat $MASTER_IP_FILE`
 fi
 
-echo ""
 echo "** Will now install a DC/OS bootstrap node with the following parameters:"
 echo ""
 echo "*****************************          ****************"
