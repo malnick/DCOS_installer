b2563393155c472c9c5d938086122cef5a49a5bc
Fernando Sanchez <fernando@mesosphere.io>
2016-11-18 19:55:41 -0500

    Update dcos_install_centos7.sh

--- dcos_install_centos7.sh
+++ dcos_install_centos7.sh
@@ -153,7 +153,7 @@ echo "**************************************************************************
                  ;;
             [9]) read -p "Enter new value for DNS server: " DNS_SERVER
                  ;;  
-            [0]) if [ "$INSTALL_ELK" = false ]; then INSTALL_ELK=true; else INSTALL_ELK=false; fi
+            [0]) if [ "$INSTALL_ELK" == false ]; then INSTALL_ELK=true; else INSTALL_ELK=false; fi
                  ;;
               *) echo "** Invalid input. Please choose an option [1-0]"
                  ;;
