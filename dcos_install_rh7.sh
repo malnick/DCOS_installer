ca23cc08f583f4758af6ab4130f649c88eefa2f9
Fernando Sanchez <fernando@mesosphere.io>
2016-11-20 19:18:07 -0500

    add jq epel-release pip and git

--- dcos_install_rh7.sh
+++ dcos_install_rh7.sh
@@ -189,7 +189,11 @@ gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
 #docker engine with selinux and other requirements
-sudo yum install -y docker-engine-1.11.2-1.el7.centos docker-engine-selinux-1.12.1-1.el7.centos.noarch wget curl zip unzip ipset ntp screen
+sudo yum install -y epel-release
+sudo yum install -y docker-engine-1.11.2-1.el7.centos docker-engine-selinux-1.12.1-1.el7.centos.noarch \
+wget curl zip unzip ipset ntp screen git python-pip python34 jq nginx
+curl https://bootstrap.pypa.io/get-pip.py | python3.4
+pip3 install --upgrade pip jsonschema
 
 #configure ntp
 sudo echo "server $NTP_SERVER" > /etc/ntp.conf && \
